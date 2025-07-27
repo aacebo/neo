ef34f047cc1e214d8210a54ef7d9859e9863f2c8
aacebo <aacebowork@gmail.com>
2025-07-26 21:05:02 -0400

    stuff

--- neo_tensor/tests/add.rs
+++ neo_tensor/tests/add.rs
@@ -5,19 +5,14 @@ pub fn should_add_bool_tensor_to_uint8_tensor() {
     let a = Bool::<2, 2>::from(true);
     let b = UInt8::<2, 2>::from(2);
     let out = b + a.to_u8();
-
-    for i in 0..2 {
-        for j in 0..2 {
-            assert_eq!(out[i][j], 3);
-        }
-    }
-
+    assert!(out == UInt8::from(3));
     println!("{}", out);
 }
 
 #[test]
 pub fn should_add_uint8_to_uint8_tensor() {
-    let a = UInt8::<2, 2>::from(2);
+    let a = UInt8::<3, 3>::from(2);
     let out = a + 2;
-    assert!(out == UInt8::<2, 2>::from(4));
+    assert!(out == UInt8::from(4));
+    println!("{}", out);
 }
--- neo_tensor/tests/mult.rs
+++ neo_tensor/tests/mult.rs
@@ -5,10 +5,5 @@ pub fn should_mult_bool_uint8() {
     let a = Bool::<2, 2>::from(true);
     let b = UInt8::<2, 2>::from(3);
     let out = b * a.to_u8();
-
-    for i in 0..2 {
-        for j in 0..2 {
-            assert_eq!(out[i][j], 6);
-        }
-    }
+    assert!(out == UInt8::from(6));
 }
